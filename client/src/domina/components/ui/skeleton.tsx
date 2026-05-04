c125ee6197146a677730d12b04b82e0839b1727d
VivaWebDesigns <m.carney3002@gmail.com>
2026-05-04 13:46:41 -0400

    Deduplicate tier site UI components

--- client/src/domina/components/ui/skeleton.tsx
+++ client/src/domina/components/ui/skeleton.tsx
@@ -1,15 +1,1 @@
-import { cn } from "@domina/lib/utils"
-
-function Skeleton({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
-      {...props}
-    />
-  )
-}
-
-export { Skeleton }
+export * from "@/components/ui/skeleton";
